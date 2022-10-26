0245b5642d4627ed7e4b37177bf8b6c1241c0bfc
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-26 14:22:31 -0400

    Completed basic login functionality for the site

--- controllers/game/gameRoutes.js
+++ controllers/game/gameRoutes.js
@@ -4,7 +4,7 @@ const Game = require("../../models/Game");
 router.get('/:game_id', async (req, res)=>{
     const gameData = await Game.findByPk(req.params.game_id);
     const game = gameData.get({plain: true});
-    res.render("gamepage", game);
+    res.render("gamepage", {game, logged_in: req.session.logged_in});
 });
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,16 @@ const Game = require("../models/Game");
 router.get('/', async (req, res) => {
     const gamesData = await Game.findAll();
     const games = gamesData.map(game => game.get({plain: true}));
-    res.render('homepage', {games});
+    res.render('homepage', {games, logged_in: req.session.logged_in});
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
