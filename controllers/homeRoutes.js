73c99eed50fc2ba68888d66e1afd3ae7700c8092
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-26 13:17:02 -0400

    Added basic HTML and styling for the homepage layout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,47 @@
 const router = require('express').Router();
+const Game = require("../models/Game");
+
+const games = [
+    {
+        name: "Game 1",
+        description: "Some game",
+        link: "6"
+    },
+    {
+        name: "Game 2",
+        description: "Some game",
+        link: "5"
+    },
+    {
+        name: "Game 3",
+        description: "Some game",
+        link: "4"
+    },
+    {
+        name: "Game 1",
+        description: "Some game",
+        link: "3"
+    },
+    {
+        name: "Game 2",
+        description: "Some game",
+        link: "2"
+    },
+    {
+        name: "Game 3",
+        description: "Some game",
+        id: "1"
+    },
+]
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    res.render('homepage', {games});
+});
+
+router.get('/:game_id', async (req, res)=>{
+    const gameData = await Game.findByPk(req.params.id);
+    const game = gameData.get({plain: true});
+    res.render("gamepage", game);
 });
 
 module.exports = router;
