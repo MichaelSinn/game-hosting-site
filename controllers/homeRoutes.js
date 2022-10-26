c9e229656c1378dc98850194557cabeab31a9823
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-26 13:49:28 -0400

    Added images to the game database, also added basic styling to the homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,40 +1,9 @@
 const router = require('express').Router();
 const Game = require("../models/Game");
 
-const games = [
-    {
-        name: "Game 1",
-        description: "Some game",
-        id: "6"
-    },
-    {
-        name: "Game 2",
-        description: "Some game",
-        id: "5"
-    },
-    {
-        name: "Game 3",
-        description: "Some game",
-        id: "4"
-    },
-    {
-        name: "Game 1",
-        description: "Some game",
-        id: "3"
-    },
-    {
-        name: "Game 2",
-        description: "Some game",
-        id: "2"
-    },
-    {
-        name: "Game 3",
-        description: "Some game",
-        id: "1"
-    },
-]
-
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const gamesData = await Game.findAll();
+    const games = gamesData.map(game => game.get({plain: true}));
     res.render('homepage', {games});
 });
 
