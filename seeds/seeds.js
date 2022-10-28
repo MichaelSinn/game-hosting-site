a9ba6466f8f4a066b7c7a10a24fe254223b293b3
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-28 15:20:06 -0400

    Updated the leaderboard so it only uses a two queries and will get all games scores

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -4,26 +4,24 @@ const { Game, User, Scores } = require('../models');
 
 const gameData = require('./gameData.json');
 const userData = require('./userData.json');
-const scoresData = require('./scoresData.json')
+const scoresData = require('./scoresData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
     await Game.bulkCreate(gameData, {
-        individualHooks: true,
         returning: true,
     });
 
     await User.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
-    })
+    });
 
     await Scores.bulkCreate(scoresData, {
         individualHooks: true,
         returning: true,
-    })
-
+    });
 
     process.exit(0);
 };
