dfd28f626d72ebb5a900dd444a98eb1b4631d384
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-30 19:54:53 -0400

    Moved snake game into one file

--- public/js/games/canvas-snake.js
+++ public/js/games/canvas-snake.js
@@ -1,8 +1,8 @@
-let submitEl = document.getElementById("submitEl")
-let scoreEl = document.getElementById("score")
-let submit = document.getElementById("submitScore")
-let playAgain = document.getElementById("playAgain")
-let continueEl = document.getElementById("continue")
+let submitEl = document.getElementById('submitEl');
+let scoreEl = document.getElementById('score');
+let submit = document.getElementById('submitScore');
+let playAgain = document.getElementById('playAgain');
+let continueEl = document.getElementById('continue');
 let score;
 let snake;
 let res = 20;
@@ -10,78 +10,141 @@ let food;
 let w;
 let h;
 
-function setup(){
-    let canvas = createCanvas(400, 400);
-    canvas.parent('gameCanvas')
-    w = floor(width / res);
-    h = floor(height / res);
-    frameRate(12)
-    snake = new Snake();
-    foodLocation()
+class Snake {
+	constructor() {
+		this.body = [];
+		this.body[0] = createVector(floor(w / 2), floor(h / 2));
+		this.xdir = 0;
+		this.ydir = 0;
+		this.len = 0;
+	}
+
+	setDir(x, y) {
+		this.xdir = x;
+		this.ydir = y;
+	}
+
+	update() {
+		let head = this.body[this.body.length - 1].copy();
+		this.body.shift();
+		head.x += this.xdir;
+		head.y += this.ydir;
+		this.body.push(head);
+	}
+
+	grow() {
+		let head = this.body[this.body.length - 1].copy();
+		this.len++;
+		this.body.push(head);
+	}
+
+	death() {
+		let x = this.body[this.body.length - 1].x;
+		let y = this.body[this.body.length - 1].y;
+		if (x > w - 1 || x < 0 || y > h - 1 || y < 0) {
+			return true;
+		}
+		for (let i = 0; i < this.body.length - 1; i++) {
+			let part = this.body[i];
+			if (part.x === x && part.y === y) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	show() {
+		for (let i = 0; i < this.body.length; i++) {
+			fill(100, 255, 218);
+			noStroke();
+			rect(this.body[i].x, this.body[i].y, 1, 1);
+		}
+	}
+
+
+	eat(pos) {
+		let x = this.body[this.body.length - 1].x;
+		let y = this.body[this.body.length - 1].y;
+		if (x === pos.x && y === pos.y) {
+			this.grow();
+			return true;
+		}
+		return false;
+	}
+}
+
+function setup() {
+	let canvas = createCanvas(400, 400);
+	canvas.parent('gameCanvas');
+	w = floor(width / res);
+	h = floor(height / res);
+	frameRate(12);
+	snake = new Snake();
+	foodLocation();
 }
 
 function foodLocation() {
-    let x = floor(random(w))
-    let y = floor(random(h))
-    food = createVector(x,y)
+	let x = floor(random(w));
+	let y = floor(random(h));
+	food = createVector(x, y);
 }
 
 function keyPressed() {
-    if (keyCode === UP_ARROW){
-        snake.setDir(0, -1)
-    } else if (keyCode === DOWN_ARROW){
-        snake.setDir(0, 1)
-    } else if (keyCode === RIGHT_ARROW){
-        snake.setDir(1, 0)
-    } else if (keyCode === LEFT_ARROW){
-        snake.setDir(-1, 0)
-    }
+	if (keyCode === UP_ARROW) {
+		snake.setDir(0, -1);
+	} else if (keyCode === DOWN_ARROW) {
+		snake.setDir(0, 1);
+	} else if (keyCode === RIGHT_ARROW) {
+		snake.setDir(1, 0);
+	} else if (keyCode === LEFT_ARROW) {
+		snake.setDir(-1, 0);
+	}
 }
 
-function draw(){
-    scale(res)
-    background(0, 0, 0);
-    if (snake.eat(food)) {
-        foodLocation()
-    }
-    snake.update();
-    snake.show();
-    if (snake.death()) {
-        submitEl.style.display = "block"
-        scoreEl.innerHTML = (snake.body.length)
-        score = snake.body.length
-        noLoop();
-    }
+function draw() {
+	scale(res);
+	background(0, 0, 0);
+	if (snake.eat(food)) {
+		foodLocation();
+	}
+	snake.update();
+	snake.show();
+	if (snake.death()) {
+		submitEl.style.display = 'block';
+		scoreEl.innerHTML = (snake.body.length);
+		score = snake.body.length;
+		noLoop();
+	}
 
-    noStroke();
-    fill(255, 255, 0);
-    rect(food.x, food.y, 1, 1);
+	noStroke();
+	fill(255, 255, 0);
+	rect(food.x, food.y, 1, 1);
 }
 
 const postScore = async function () {
-    const response = await fetch(`${window.location.href}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({score})
-    });
-    if (response.ok) {
-        console.log("Score added");
-    }
-}
+	const response = await fetch(`${window.location.href}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({score})
+	});
+	if (response.ok) {
+		console.log('Score added');
+	}
+};
 
 
-submit.addEventListener("click", () => {
-    postScore()
-        .then((data) => console.log('score added'))
-        .catch((err) => console.log(err))
-    submitEl.style.display = "none"
-    playAgain.style.display = "block"
-})
+submit.addEventListener('click', () => {
+	postScore()
+	  .then((data) => console.log('score added'))
+	  .catch((err) => console.log(err));
+	submitEl.style.display = 'none';
+	playAgain.style.display = 'block';
+});
 
-continueEl.addEventListener("click", () => {
-    playAgain.style.display = "none"
-    location.reload()
-})
+continueEl.addEventListener('click', () => {
+	playAgain.style.display = 'none';
+	location.reload();
+});
 
