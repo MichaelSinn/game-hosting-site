b18da016f73061f517dc428a5700f24e78fbc370
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-31 10:48:16 -0400

    Added instructions to the pig and timmy games

--- models/Game.js
+++ models/Game.js
@@ -1,44 +1,45 @@
-const { Model, DataTypes } = require('sequelize');
+const {Model, DataTypes} = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 const Score = require('./Scores');
 
-class Game extends Model {}
+class Game extends Model {
+}
 
-Game.init (
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        script: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        hero_image: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            defaultValue: "coming-soon.jpg"
-        },
-        instructions: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-            defaultValue: "Coming Soon"
-        }
-    },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'game',
-    }
+Game.init(
+  {
+	  id: {
+		  type: DataTypes.INTEGER,
+		  allowNull: false,
+		  primaryKey: true,
+		  autoIncrement: true,
+	  },
+	  name: {
+		  type: DataTypes.STRING,
+		  allowNull: false,
+	  },
+	  script: {
+		  type: DataTypes.STRING,
+		  allowNull: false
+	  },
+	  hero_image: {
+		  type: DataTypes.STRING,
+		  allowNull: false,
+		  defaultValue: 'coming-soon.jpg'
+	  },
+	  instructions: {
+		  type: DataTypes.TEXT,
+		  allowNull: false,
+		  defaultValue: 'Coming Soon'
+	  }
+  },
+  {
+	  sequelize,
+	  timestamps: false,
+	  freezeTableName: true,
+	  underscored: true,
+	  modelName: 'game',
+  }
 );
 
-module.exports = Game
+module.exports = Game;
